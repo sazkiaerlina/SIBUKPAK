baa27ed52c14f0490bb5cd7cc56675d889601ffa
Vania <vaniaraissaandini@gmail.com>
2026-07-11 17:41:01 +0700

    perbaikan tombl register lagi

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -13,7 +13,7 @@
         </div>
 
         <!-- Teks -->
-        <div class="w-full lg:w-1/2 text-center lg:text-left flex flex-col items-center lg:items-start">
+        <div class="w-full lg:w-1/2 text-center lg:text-left">
 
            
 <div class="flex items-center justify-center lg:justify-start gap-4 mb-6">
@@ -30,7 +30,7 @@
 </div>
 
 
-            <p class="text-lg sm:text-xl text-gray-700 leading-8 sm:leading-9 max-w-xl mx-auto lg:mx-0">
+           <p class="text-lg sm:text-xl text-gray-700 leading-8 sm:leading-9 max-w-xl mx-auto lg:mx-0 text-center lg:text-left">
                 Statistik Berdampak Untuk Kampus Berdampak (SIBUKPAK) 
                 merupakan platform Badan Pusat Statistik Kabupaten Ogan Ilir
                 yang mendukung digitalisasi proses magang mahasiswa. Melalui website ini, 
@@ -38,13 +38,13 @@
                 melakukan absensi harian secara mudah setelah diterima sebagai peserta magang di BPS Kabupaten Ogan Ilir.
             </p>
 
-            <a
-                href="{{ route('register') }}"
-                class="inline-block mt-10 bg-[#043277] hover:bg-[#0D3D88] text-white text-xl px-14 py-5 rounded-2xl shadow-lg transition">
-
-                Daftar Magang
-
-            </a>
+          <div class="mt-10 text-center lg:text-left">
+    <a
+        href="{{ route('register') }}"
+        class="inline-block bg-[#043277] hover:bg-[#0D3D88] text-white text-xl px-10 py-4 rounded-2xl shadow-lg transition">
+        Daftar Magang
+    </a>
+</div>
 
         </div>
 
